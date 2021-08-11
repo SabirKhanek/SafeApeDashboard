66206f7d374f6e9f55688727dcace5af1fa21927
Sabir <47791141+SabirKhanek@users.noreply.github.com>
2021-08-11 18:55:43 +0500

    Initial COMMIT

---
+++ index.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/query', methods=['POST', 'GET'])
+def getquery():
+    wallet_address = request.form['address']
+    print(wallet_address)
+    return render_template("index.html", Address= wallet_address, Dividends= "10000 BNB", Unlock_time="13:10 UTC")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
